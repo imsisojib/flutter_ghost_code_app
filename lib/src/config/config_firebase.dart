5f42dbca56e2234df811df059cc93275442cdd82
imsisojib <sirajulislam1123@gmail.com>
2024-05-26 01:10:24 +0600

    firebase config list updated.

--- lib/src/config/config_firebase.dart
+++ lib/src/config/config_firebase.dart
@@ -1,4 +1,9 @@
 class ConfigFirebase{
   //firebase-tables
   static const String tableUsers = "users";
+  static const String tableAvatars = "avatars";
+
+  //storage_folders
+  static const String storageAvatars = "avatars";
+  static const String storageProfilePictures = "profile_pictures";
 }
