25d5daa843339df9c81002429cb1ccdfa3da04f4
imsisojib <sirajulislam1123@gmail.com>
2024-05-29 15:29:30 +0600

    merchandise domain added.

--- lib/src/config/config_firebase.dart
+++ lib/src/config/config_firebase.dart
@@ -2,8 +2,10 @@ class ConfigFirebase{
   //firebase-tables
   static const String tableUsers = "users";
   static const String tableAvatars = "avatars";
+  static const String tableShop = "shop";
 
   //storage_folders
   static const String storageAvatars = "avatars";
   static const String storageProfilePictures = "profile_pictures";
+  static const String storageShop = "shop";
 }
